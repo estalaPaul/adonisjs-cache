061a1516a6e5603d7fe3a8923330d9c67497b8fe
Paul Estala <pestala495@tutanota.com>
2022-04-10 10:14:36 -0600

    test: add test for invalid cache entry

--- tests/stores/file/get.spec.ts
+++ tests/stores/file/get.spec.ts
@@ -1,5 +1,19 @@
 import { test } from '@japa/runner'
+import { rm, writeFile } from 'fs/promises'
 import FileStore from '../../../src/Stores/File'
+import crypto from 'crypto'
+
+test('get invalid cache entry', async ({ expect }) => {
+    const fileStore = new FileStore('./.tmp')
+    const path = `./.tmp/cache/${crypto
+        .createHash('sha1')
+        .update('random')
+        .digest('base64')}`
+    await writeFile(path, JSON.stringify({ test: 'testing' }))
+
+    expect(await fileStore.get('random')).toBeNull()
+    await rm(path)
+})
 
 test('can get non existent cache entry', async ({ expect }) => {
     const fileStore = new FileStore('./.tmp')
